2a0ca80ba3aaab1db814650c8d437b5ba69454a6
Cyvern <cyverneon@gmail.com>
2025-03-16 18:20:56 +0000

    Reapply "Update app.js"
    This reverts commit 2d467c78cebd62bbdeeae80b098e9eb107e5f557.

--- src/js/app.js
+++ src/js/app.js
@@ -6,23 +6,23 @@ document.addEventListener("DOMContentLoaded", function () {
   particlesJS('particles-js', {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 40,
         "density": {
           "enable": true,
           "value_area": 800
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#dddddd"
       },
       "shape": {
-        "type": "polygon",
+        "type": "circle",
         "stroke": {
           "width": 0,
           "color": "#000000"
         },
         "polygon": {
-          "nb_sides": 5
+          "nb_sides": 3
         },
         "image": {
           "src": "img/github.svg",
@@ -45,23 +45,23 @@ document.addEventListener("DOMContentLoaded", function () {
         "random": true,
         "anim": {
           "enable": false,
-          "speed": 19.18081918081918,
+          "speed": 40,
           "size_min": 0.1,
           "sync": false
         }
       },
       "line_linked": {
         "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
+        "distance": 120,
+        "color": "#dddddd",
         "opacity": 0.4,
         "width": 1
       },
       "move": {
         "enable": true,
-        "speed": 4,
-        "direction": "none",
-        "random": true,
+        "speed": 2,
+        "direction": "bottom",
+        "random": false,
         "straight": false,
         "out_mode": "out",
         "bounce": false,
@@ -77,18 +77,18 @@ document.addEventListener("DOMContentLoaded", function () {
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": false,
+          "enable": true,
           "mode": "grab"
         },
         "onclick": {
-          "enable": true,
+          "enable": false,
           "mode": "push"
         },
         "resize": true
       },
       "modes": {
         "grab": {
-          "distance": 400,
+          "distance": 140,
           "line_linked": {
             "opacity": 1
           }
